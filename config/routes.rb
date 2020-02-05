6cfc88998fbd1df28584a878c363f15bf29f375d
Rucheng Xiao <rucheng.xiao@appfolio.com>
2020-02-05 13:44:56 -0800

    RX - Add ':only' delimiter to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :images
-  get 'application/home'
+  resources :images, :only => [:index, :show, :new, :create]
   root 'application#home'
 end

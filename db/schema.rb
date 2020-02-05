28897a3e694614cf8a0d3900001eab508c879cbc
Rucheng Xiao <rucheng.xiao@appfolio.com>
2020-02-05 13:16:42 -0800

    RX - Change herf to link and remove "null: false"

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2020_02_04_230356) do
   create_table "images", force: :cascade do |t|
     t.string "link"
     t.string "tag"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

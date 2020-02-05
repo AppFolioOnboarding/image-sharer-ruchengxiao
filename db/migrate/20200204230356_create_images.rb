a3628ff22ae23fc0f368c2a1871608997f3df93f
Rucheng Xiao <rucheng.xiao@appfolio.com>
2020-02-04 20:11:11 -0800

    RX - Add image controller, model and corresponding page

---
+++ db/migrate/20200204230356_create_images.rb
@@ -0,0 +1,10 @@
+class CreateImages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :images do |t|
+      t.string :link, null:false
+      t.string :tag, null:false
+
+      t.timestamps
+    end
+  end
+end

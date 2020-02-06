a90ff84652b1c47fede4115b43b5a61926326f93
Rucheng Xiao <rucheng.xiao@appfolio.com>
2020-02-05 16:32:45 -0800

    RX - Rollback and run the migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_02_04_230356) do
 
   create_table "images", force: :cascade do |t|
-    t.string "link"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "link", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

cfc0899a01e4e5113fc65e2d31c8811a55f32a7a
Rucheng Xiao <rucheng.xiao@appfolio.com>
2020-02-06 15:42:30 -0800

    RX - Added the tag function

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,8 @@
 class Image < ApplicationRecord
   validates :link, presence: true
   validate :check_link
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
+  acts_as_taggable_on :skills, :interests
 
   private
 
